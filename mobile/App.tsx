7c1b8689278a024db3731cdce3a4ec4e034add39
jackson-lafrance <jackson@codeflight.dev>
2025-10-29 20:10:37 -0400

    Convert React Native CLI project to Expo to avoid CocoaPods requirement

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/services/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <AuthProvider>
-      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      <StatusBar style="auto" />
       <AppNavigator />
     </AuthProvider>
   );
